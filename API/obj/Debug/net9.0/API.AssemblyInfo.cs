afd198ccb9c3a21edd04e1b7813db21abee37af7
SUBHAM ROUT <subhamrout.max23@gmail.com>
2026-08-18 03:23:28 +0530

    added services & 90 percent completed backend

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ab4fef54629e4f457e3bd13ca263dc46707e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f9a92a1627ebec0c3c3584f41dd953fb8a3d549")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
